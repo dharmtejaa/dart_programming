ee883d68f30b1774277cf4545d3b4c51fc7124b1
Tej <dharmateja238@gmail.com>
2025-10-14 12:45:06 +0530

    program to use constant

--- bin/stockmanagement.dart
+++ bin/stockmanagement.dart
@@ -4,5 +4,6 @@ void main() {
   // print("Enter Your Name : ");
   // String? name = stdin.readLineSync();
   // print("Hello $name, Welcome to dart programming language world");
-  print("Hello I am 'Dharma Teja'");
+  const int number = 10;
+  print("$number");
 }
