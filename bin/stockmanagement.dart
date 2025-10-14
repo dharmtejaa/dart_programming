474089b2bd107249f6bd2faa346c7b6e962fbd3d
Tej <dharmateja238@gmail.com>
2025-10-14 16:19:20 +0530

    dart program to check if the number is odd or even.

--- bin/stockmanagement.dart
+++ bin/stockmanagement.dart
@@ -1,10 +1,8 @@
 import 'dart:io';
 
+//Write a dart program to check if the number is odd or even.
 void main() {
-  print("Enter the values of P, T and R");
-  int p = int.parse(stdin.readLineSync()!);
-  int t = int.parse(stdin.readLineSync()!);
-  int r = int.parse(stdin.readLineSync()!);
-  double si = (p * t * r) / 100;
-  print("the simple interest is ${si.toStringAsFixed(2)}");
+  print("Enter a number : ");
+  int? number = int.parse(stdin.readLineSync()!);
+  (number % 2 == 0) ? print("$number is Even") : print("$number is Odd");
 }
