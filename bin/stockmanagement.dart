7797438f4006bc4d5fad2ecb6510f5e595183047
Tej <dharmateja238@gmail.com>
2025-10-14 17:04:49 +0530

    dart program to create a simple calculator that performs addition, subtraction, multiplication, and division.

--- bin/stockmanagement.dart
+++ bin/stockmanagement.dart
@@ -1,9 +1,40 @@
-//Write a dart program to generate multiplication tables of 1 - 9.
+import 'dart:io';
+
+//Write a dart program to create a simple calculator that performs addition, subtraction, multiplication, and division.
 void main() {
-  for (int i = 1; i <= 9; i++) {
-    print("Multiplication Table of $i");
-    for (int j = 1; j <= 10; j++) {
-      print("$i x $j = ${i * j}");
+  bool exit = true;
+  while (exit) {
+    print("\nEnter the operation to be performed :");
+    print("1.Addition 2.Subtraction 3.Multiplication 4.Division 5.Exit");
+    int? operation = int.parse(stdin.readLineSync()!);
+    if (operation == 5) {
+      exit = false;
+      print("exiting...");
+      break;
+    }
+    print("Enter first Number :");
+    int? num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second Number :");
+    int? num2 = int.parse(stdin.readLineSync()!);
+    switch (operation) {
+      case 1:
+        print("\n${num1 + num2}");
+        break;
+      case 2:
+        print("\n${num1 - num2}");
+        break;
+      case 3:
+        print("\n${num1 * num2}");
+        break;
+      case 4:
+        if (num2 == 0) {
+          print("\nError: Division by zero is not allowed.");
+        } else {
+          print("\n${num1 / num2}");
+        }
+        break;
+      default:
+        print("Invalid Operation");
     }
   }
 }
