3e059bfd9570267ecdfb4f70e32042f8b6120b4d
Tej <dharmateja238@gmail.com>
2025-10-14 12:46:11 +0530

    program in Dart that finds simple interest. Formula= (p * t * r) / 100

--- bin/stockmanagement.dart
+++ bin/stockmanagement.dart
@@ -1,9 +1,10 @@
-//import 'dart:io';
+import 'dart:io';
 
 void main() {
-  // print("Enter Your Name : ");
-  // String? name = stdin.readLineSync();
-  // print("Hello $name, Welcome to dart programming language world");
-  const int number = 10;
-  print("$number");
+  print("Enter the values of P, T and R");
+  int p = int.parse(stdin.readLineSync()!);
+  int t = int.parse(stdin.readLineSync()!);
+  int r = int.parse(stdin.readLineSync()!);
+  double si = (p * t * r) / 100;
+  print("the simple interest is ${si.toStringAsFixed(2)}");
 }
