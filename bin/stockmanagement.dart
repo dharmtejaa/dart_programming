cb225639dcf5cbc7e647a751a0cb8cbb7a7751c9
Tej <dharmateja238@gmail.com>
2025-10-14 16:27:19 +0530

    dart program to check whether a number is positive, negative, or zero.

--- bin/stockmanagement.dart
+++ bin/stockmanagement.dart
@@ -1,21 +1,14 @@
 import 'dart:io';
 
-//Write a dart program to check whether a character is vowel or consonant.
+//Write a dart program to check whether a number is positive, negative, or zero.
 void main() {
-  print("Enter a character : ");
-  String? char = stdin.readLineSync();
-  if (char == 'a' ||
-      char == 'e' ||
-      char == 'i' ||
-      char == 'o' ||
-      char == 'u' ||
-      char == 'A' ||
-      char == 'E' ||
-      char == 'I' ||
-      char == 'O' ||
-      char == 'U') {
-    print("$char is a Vowel");
+  print("Enter a number : ");
+  int? number = int.parse(stdin.readLineSync()!);
+  if (number >= 1) {
+    print("$number is a positive number");
+  } else if (number < 0) {
+    print("$number is a negative number");
   } else {
-    print("$char is a Consonant");
+    print("It's a $number");
   }
 }
