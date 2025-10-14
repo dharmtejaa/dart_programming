4be974a2c9ced0a1daddbf99bfae1c22b0959d20
Tej <dharmateja238@gmail.com>
2025-10-14 16:23:20 +0530

    dart program to check dart program to check whether a character is a vowel or consonant.

--- bin/stockmanagement.dart
+++ bin/stockmanagement.dart
@@ -1,8 +1,21 @@
 import 'dart:io';
 
-//Write a dart program to check if the number is odd or even.
+//Write a dart program to check whether a character is vowel or consonant.
 void main() {
-  print("Enter a number : ");
-  int? number = int.parse(stdin.readLineSync()!);
-  (number % 2 == 0) ? print("$number is Even") : print("$number is Odd");
+  print("Enter a character : ");
+  String? char = stdin.readLineSync();
+  if (char == 'a' ||
+      char == 'e' ||
+      char == 'i' ||
+      char == 'o' ||
+      char == 'u' ||
+      char == 'A' ||
+      char == 'E' ||
+      char == 'I' ||
+      char == 'O' ||
+      char == 'U') {
+    print("$char is a Vowel");
+  } else {
+    print("$char is a Consonant");
+  }
 }
