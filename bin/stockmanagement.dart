a90663b695ab81484b9d02ef939661da1110d094
Tej <dharmateja238@gmail.com>
2025-10-15 16:40:25 +0530

    Dart program called maxNumber that takes three numbers as arguments and returns the largest number.

--- bin/stockmanagement.dart
+++ bin/stockmanagement.dart
@@ -1,10 +1,21 @@
-//Write a function in Dart named add that takes two numbers as arguments and returns their sum.
+//Write a function in Dart called maxNumber that takes three numbers as arguments and returns the largest number.
 void main() {
-  int num1 = 5;
-  int num2 = 3;
-  print(add(num1, num2));
+  int num1 = 15;
+  int num2 = 30;
+  int num3 = 8;
+  print(findGreatestNumber(num1, num2, num3));
 }
 
-int add(int num1, int num2) {
-  return num1 + num2;
+int findGreatestNumber(int num1, int num2, int num3) {
+  if (num1 >= num2) {
+    if (num1 >= num3) {
+      return num1;
+    } else {
+      return num3;
+    }
+  } else if (num2 >= num3) {
+    return num2;
+  } else {
+    return num3;
+  }
 }
