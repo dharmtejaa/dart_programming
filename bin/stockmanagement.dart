759bfa89efd91cc3c40c4630ab672623b8c29ade
Tej <dharmateja238@gmail.com>
2025-10-15 16:33:10 +0530

    file ranamed

---
+++ bin/stockmanagement.dart
@@ -0,0 +1,10 @@
+//Write a function in Dart named add that takes two numbers as arguments and returns their sum.
+void main() {
+  int num1 = 5;
+  int num2 = 3;
+  print(add(num1, num2));
+}
+
+int add(int num1, int num2) {
+  return num1 + num2;
+}
