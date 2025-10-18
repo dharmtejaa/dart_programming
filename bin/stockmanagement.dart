d7f694853103e204d9fa4e81c15809745b55af4f
Tej <dharmateja238@gmail.com>
2025-10-18 09:43:18 +0530

    Write a dart program to add your name to hello.text file.

--- bin/stockmanagement.dart
+++ bin/stockmanagement.dart
@@ -1,48 +1,10 @@
-//Create a simple to-do application that allows user to add, remove, and view their task.
+//Write a dart program to add your name to “hello.txt” file.
 import 'dart:io';
 
 void main() {
-  Map<int, String> tasks = {};
-  print("Welcome to the To-Do Application!");
-  int count = 0;
-  while (true) {
-    print("Choose an option: 1. Add Task 2. Remove Task 3. View Tasks 4. Exit");
-    int? choice = int.parse(stdin.readLineSync()!);
-    if (choice == 4) {
-      break;
-    } else {
-      switch (choice) {
-        case 1:
-          print("Enter the task to add:");
-          String? taskToAdd = stdin.readLineSync();
-          count += 1;
-          tasks[count] = taskToAdd!;
-          print("Task added.");
-          break;
-        case 2:
-          print("Enter the task number to remove: ");
-          int? taskNumberToRemove = int.parse(stdin.readLineSync()!);
-          if (tasks.containsKey(taskNumberToRemove)) {
-            tasks.remove(taskNumberToRemove);
-            print("Task removed.");
-          } else {
-            print("Task not found.");
-          }
-          break;
-        case 3:
-          if (tasks.isEmpty) {
-            print("No tasks available.");
-            break;
-          }
-          print("Your Tasks:");
-          tasks.forEach((key, value) {
-            print("$key: $value");
-          });
-          break;
-        default:
-          print("Invalid choice. Please try again.");
-          break;
-      }
-    }
-  }
+  File file = File('hello.text');
+  file.writeAsStringSync('Dharma Teja\n', mode: FileMode.append);
+  print('Name added to hello.txt');
+  print('File path: ${file.absolute.path}');
+  print(file.readAsStringSync());
 }
