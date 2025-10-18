2169142c70e91a2aa7b7cc8cbf0e68b0145105bf
Tej <dharmateja238@gmail.com>
2025-10-18 13:19:20 +0530

    Write a dart program to create 100 files using loop.

--- bin/stockmanagement.dart
+++ bin/stockmanagement.dart
@@ -1,17 +1,10 @@
-//Write a dart program to copy the “hello.txt” file to “hello_copy.txt” file.
+//Write a dart program to create 100 files using loop.
 import 'dart:io';
 
 void main() {
-  final sourceFile = File('hello.text');
-  final destinationFile = File('hello_copy.txt');
-  sourceFile
-      .copy(destinationFile.path)
-      .then((file) {
-        print('File copied to ${file.path}');
-        print("the copied file content is:");
-        print(destinationFile.readAsStringSync());
-      })
-      .catchError((e) {
-        print('Error occurred: $e');
-      });
+  for (int i = 1; i <= 100; i++) {
+    File file = File('file_$i.txt');
+    file.writeAsStringSync('This is file number $i');
+  }
+  print('100 files created successfully.');
 }
